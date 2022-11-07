677423a8f1e3d7daa6b6f9abcef9aef5509d230f
User01 <romansochirca30@gmail.com>
2022-11-07 19:31:48 +0200

    Add new functionality

--- src/script.js
+++ src/script.js
@@ -1,11 +1,23 @@
-fetch('https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=6587ef0788721dcbcc61ec44fa9751df')
-    .then(function (resp) { return resp.json() })
-    .then(function (data) {
-        console.log(data);
-        document.querySelector('.card-title').innerHTML = `${data.name} ${Math.round(data.main.temp - 273)}&deg;`;
-        document.querySelector('.card-text').textContent = data.weather[0]['description'];
-        document.querySelector('.imagine').innerHTML = `<img src= "https://openweathermap.org/img/wn/${data.weather[0]['icon']}@2x.png">`;
-    })
-    .catch(function () {
+const input = document.querySelector(".form-control");
+const btn = document.querySelector(".btn");
+
+btn.addEventListener("click", () => {
+    let City = input.value;
+
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${City}&appid=6587ef0788721dcbcc61ec44fa9751df`)
+        .then(function (resp) { return resp.json() })
+        .then(function (data) {
+            if (data.cod != 200) {
+                document.querySelector('.card-title').innerHTML = "Oops! The requested city could not be found...";
+                document.querySelector('.card-text').textContent = "";
+                document.querySelector('.imagine').innerHTML = `<img src= "./img/img.png">`;
+            } else {
+                document.querySelector('.card-title').innerHTML = `${data.name} ${Math.round(data.main.temp - 273)}&deg;`;
+                document.querySelector('.card-text').textContent = data.weather[0]['description'];
+                document.querySelector('.imagine').innerHTML = `<img src= "https://openweathermap.org/img/wn/${data.weather[0]['icon']}@2x.png">`;
+            }
+        })
+        .catch(function () {
 
     });
+})
